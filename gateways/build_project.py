9aac0ae1e14899ced643611684118b3a54200dc1
GavrilS <gavr1lll@abv.bg>
2024-08-24 13:40:09 +0300

    Restructuring and adding file handling options for moving/removing projects to a tmp work dir

---
+++ gateways/build_project.py
@@ -0,0 +1,25 @@
+import os
+from subprocess import Popen, PIPE
+
+
+class FileGateway:
+
+    def __init__(self):
+        pass
+
+    def copy_project_to_work_dir(project_src, work_dir):
+        # cmd = f"mv -rf {project_src} {work_dir}"
+        cmd = ['mv', '-rf', project_src, work_dir]
+
+        ps = Popen(cmd, stdout=PIPE, stderr=PIPE)
+        stdout, stderr = ps.communicate()
+        print('Output of the "copy_project_to_work_dir" command: ', stdout)
+        print('Errors resulting from the command: ', stderr)
+
+    def clear_work_dir(work_dir):
+        cmd = ['rm', '-rf', work_dir]
+
+        ps = Popen(cmd, stdout=PIPE, stderr=PIPE)
+        stdout, stderr = ps.communicate()
+        print('Output of the "clean_work_dir" command: ', stdout)
+        print('Errors resulting from the command: ', stderr)
--- setup_helpers/build_project.py
+++
@@ -1,9 +0,0 @@
-import os
-from subprocess import Popen, PIPE
-
-
-def copy_project_to_work_dir(project_src, work_dir):
-    # cmd = f"mv -rf {project_src} {work_dir}"
-    cmd = ['mv', '-rf', project_src, work_dir]
-
-    ps = Popen(cmd, stdout=PIPE, stderr=PIPE)
